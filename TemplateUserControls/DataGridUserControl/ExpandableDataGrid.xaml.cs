61778ed0e67478deefc6e55764e380f77d527c06
Sanu Antony <santony@nextgen.com>
2019-08-12 17:51:01 +0530

    Width and visibility settings

--- TemplateUserControls/DataGridUserControl/ExpandableDataGrid.xaml.cs
+++ TemplateUserControls/DataGridUserControl/ExpandableDataGrid.xaml.cs
@@ -29,6 +29,7 @@ namespace TemplateUserControls.DataGridUserControl
         #region Private Properties
         private double totalWidth = 0;
         private double currentWidth = 0;
+        private ExpandableGridColumn currentVisibleColumn = new ExpandableGridColumn();
         private ExpandableGridColumn nextInlineColumn = new ExpandableGridColumn();
         #endregion Private Properties
 
@@ -39,7 +40,7 @@ namespace TemplateUserControls.DataGridUserControl
         }
 
         public static readonly DependencyProperty ColumnsListProperty =
-            DependencyProperty.Register("ColumnsList", typeof(List<ExpandableGridColumn>), typeof(ExpandableDataGrid), new PropertyMetadata(0));
+            DependencyProperty.Register("ColumnsList", typeof(List<ExpandableGridColumn>), typeof(ExpandableDataGrid), new PropertyMetadata(new List<ExpandableGridColumn>()));
 
 
         public ObservableCollection<object> ItemSourceCollection
@@ -49,7 +50,7 @@ namespace TemplateUserControls.DataGridUserControl
         }
 
         public static readonly DependencyProperty ItemSourceCollectionProperty =
-            DependencyProperty.Register("ItemSourceCollection", typeof(ObservableCollection<object>), typeof(ExpandableDataGrid), new PropertyMetadata(0));
+            DependencyProperty.Register("ItemSourceCollection", typeof(ObservableCollection<object>), typeof(ExpandableDataGrid), new PropertyMetadata(new ObservableCollection<object>()));
 
         private void DataGrid_SizeChanged(object sender, SizeChangedEventArgs e)
         {
@@ -60,22 +61,22 @@ namespace TemplateUserControls.DataGridUserControl
             }
             if (nextInlineColumn != null && nextInlineColumn.Width > 0)
                 if ((currentWidth + nextInlineColumn.Width) <= DataGridExpander.ActualWidth)
-                {
-
+                {                    
+                    ColumnsList[ColumnsList.LastIndexOf(nextInlineColumn)].Visibility = true;
+                    FindCurrentAndNextColumn();
                 }
                 else if (currentWidth>ActualWidth)
                 {
-
+                    ColumnsList[ColumnsList.LastIndexOf(currentVisibleColumn)].Visibility = false;
+                    FindCurrentAndNextColumn();
                 }
-
             currentWidth = DataGridExpander.ActualWidth;
-            
-
         }
 
-        private void FindNextColumnToShow()
+        private void FindCurrentAndNextColumn()
         {
             nextInlineColumn = ColumnsList.OrderBy(x => x.Order).ToList().FirstOrDefault(i=>i.Visibility.Equals(false));
+            currentVisibleColumn = ColumnsList.OrderBy(x => x.Order).ToList().LastOrDefault(i => i.Visibility.Equals(true));
         }
     }
 }
