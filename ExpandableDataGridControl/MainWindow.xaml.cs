139296034db4f7337a014246dcae48b8ff8f4ca5
Sanu Antony <santony@nextgen.com>
2019-08-14 14:49:29 +0530

    This is testing branch

--- ExpandableDataGridControl/MainWindow.xaml.cs
+++ ExpandableDataGridControl/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TemplateUserControls.DataGridUserControl;
 
 namespace ExpandableDataGridControl
 {
@@ -23,6 +24,50 @@ namespace ExpandableDataGridControl
         public MainWindow()
         {
             InitializeComponent();
+            //People = new List<Person>
+            //{
+            //    new Person
+            //    {
+            //        FirstName="Sanu",LastName="Antony",MiddleName="",EmailAddress="sanu@nextgen.com",PhoneNumber="9995537668"
+            //    },
+            //    new Person
+            //    {
+            //        FirstName="Anupama",LastName="S",MiddleName="P",EmailAddress="anu@nextgen.com",PhoneNumber="9741553153"
+            //    },
+            //    new Person
+            //    {
+            //        FirstName="C",LastName="Antony",MiddleName="F",EmailAddress="antony@nextgen.com",PhoneNumber="9895318686"
+            //    }
+            //};
+            //Columns = new List<ExpandableGridColumn>
+            //{
+            //    new ExpandableGridColumn
+            //    {
+            //        ColumnName ="FirstName",Header="First Name",Order=1,Visibility=true,Width=100
+            //    },
+            //    new ExpandableGridColumn
+            //    {
+            //        ColumnName ="LastName",Header="Last Name",Order=2,Visibility=true,Width=100
+            //    },
+            //    new ExpandableGridColumn
+            //    {
+            //        ColumnName ="MiddleName",Header="Middle Name",Order=3,Visibility=true,Width=100
+            //    },
+            //    new ExpandableGridColumn
+            //    {
+            //        ColumnName ="EmailAddress",Header="Email Address",Order=4,Visibility=false,Width=100
+            //    },
+            //    new ExpandableGridColumn
+            //    {
+            //        ColumnName ="PhoneNumber",Header="Phone Number",Order=5,Visibility=false,Width=100
+            //    }
+            //};
         }
+
+        public List<Person> People { get; set; }
+        public List<ExpandableGridColumn> Columns { get; set; }
+
     }
+
+
 }
--- TemplateUserControls/DataGridUserControl/ExpandableDataGrid.xaml.cs
+++ TemplateUserControls/DataGridUserControl/ExpandableDataGrid.xaml.cs
@@ -23,6 +23,7 @@ namespace TemplateUserControls.DataGridUserControl
     {
         public ExpandableDataGrid()
         {
+            ColumnsList = Columns;
             InitializeComponent();
             GenerateGridColumns();
         }
@@ -51,37 +52,80 @@ namespace TemplateUserControls.DataGridUserControl
         /// <summary>
         /// ItemSource for the datagrid
         /// </summary>
-        public ObservableCollection<object> ItemSourceCollection
+        public object ItemSourceCollection
         {
-            get { return (ObservableCollection<object>)GetValue(ItemSourceCollectionProperty); }
+            get { return (object)GetValue(ItemSourceCollectionProperty); }
             set { SetValue(ItemSourceCollectionProperty, value); }
         }
 
         public static readonly DependencyProperty ItemSourceCollectionProperty =
-            DependencyProperty.Register("ItemSourceCollection", typeof(ObservableCollection<object>), typeof(ExpandableDataGrid), new PropertyMetadata(new ObservableCollection<object>()));
+            DependencyProperty.Register("ItemSourceCollection", typeof(object), typeof(ExpandableDataGrid), new PropertyMetadata(new object()));
 
         #endregion Dependency Properties
 
         private void DataGrid_SizeChanged(object sender, SizeChangedEventArgs e)
         {
+
             if (ColumnsList != null && ColumnsList.Count > 0)
             {
                 totalWidth = ColumnsList.Sum(x => x.Width);
                 currentWidth = ColumnsList.Where(i => i.Visibility.Equals(true)).Sum(x => x.Width);
+                FindCurrentAndNextColumn();
+            }
+            else
+            {
+                Columns = new List<ExpandableGridColumn>
+            {
+                new ExpandableGridColumn
+                {
+                    ColumnName ="FirstName",Header="First Name",Order=1,Visibility=true,Width=100
+                },
+                new ExpandableGridColumn
+                {
+                    ColumnName ="LastName",Header="Last Name",Order=2,Visibility=true,Width=100
+                },
+                new ExpandableGridColumn
+                {
+                    ColumnName ="MiddleName",Header="Middle Name",Order=3,Visibility=false,Width=100
+                },
+                new ExpandableGridColumn
+                {
+                    ColumnName ="EmailAddress",Header="Email Address",Order=4,Visibility=false,Width=100
+                },
+                new ExpandableGridColumn
+                {
+                    ColumnName ="PhoneNumber",Header="Phone Number",Order=5,Visibility=false,Width=100
+                }
+            };
+                People = new ObservableCollection<Person>
+            {
+                new Person
+                {
+                    FirstName="Sanu",LastName="Antony",MiddleName="",EmailAddress="sanu@nextgen.com",PhoneNumber="9995537668"
+                },
+                new Person
+                {
+                    FirstName="Anupama",LastName="S",MiddleName="P",EmailAddress="anu@nextgen.com",PhoneNumber="9741553153"
+                },
+                new Person
+                {
+                    FirstName="C",LastName="Antony",MiddleName="F",EmailAddress="antony@nextgen.com",PhoneNumber="9895318686"
+                }
+            };
             }
             if (nextInlineColumn != null && nextInlineColumn.Width > 0)
                 if ((currentWidth + nextInlineColumn.Width) <= DataGridExpander.ActualWidth)
                 {
                     ColumnsList[ColumnsList.LastIndexOf(nextInlineColumn)].Visibility = true;
-                    FindCurrentAndNextColumn();
                 }
                 else if (currentWidth > ActualWidth)
                 {
                     ColumnsList[ColumnsList.LastIndexOf(currentVisibleColumn)].Visibility = false;
-                    FindCurrentAndNextColumn();
                 }
             currentWidth = DataGridExpander.ActualWidth;
+            FindCurrentAndNextColumn();
             GenerateGridColumns();
+            // DataGridExpander.UpdateLayout();
 
         }
         #region Methods
@@ -100,18 +144,52 @@ namespace TemplateUserControls.DataGridUserControl
         /// </summary>
         private void GenerateGridColumns()
         {
-            DataGridExpander.ItemsSource = ItemSourceCollection;
-            foreach (var item in ColumnsList.Where(x => x.Visibility.Equals(true)))
-            {
-                var column = new DataGridTextColumn
+
+            DataGridExpander.Columns.Clear();
+            DataGridExpander.ItemsSource = People;
+            if (ColumnsList != null)
+                foreach (var item in ColumnsList.Where(x => x.Visibility.Equals(true)))
                 {
-                    Header = item.Header,
-                    Binding = new Binding(item.ColumnName),
-                    Width = item.Width,
-                    Visibility = item.Visibility ? Visibility.Visible : Visibility.Collapsed
-                };
+                    var column = new DataGridTextColumn
+                    {
+                        Header = item.Header,
+                        Binding = new Binding(item.ColumnName),
+                        Width = item.Width,
+                        Visibility = item.Visibility ? Visibility.Visible : Visibility.Collapsed
+                    };
+                    DataGridExpander.Columns.Add(column);
+                }
+            if (false)
+            {
+                foreach (var item in ColumnsList.Where(x => x.Visibility.Equals(true)))
+                {
+                    for (int i = 0; i < DataGridExpander.Columns.Count; i++)
+                    {
+
+                        var z = DataGridExpander.Columns[i].ClipboardContentBinding.BindingGroupName;
+                        var q = DataGridExpander.Columns[i].ClipboardContentBinding;
+                        if (Convert.ToString(DataGridExpander.Columns[i].Header) == item.ColumnName)
+                        {
+                            DataGridExpander.Columns[i].Visibility = item.Visibility ? Visibility.Visible : Visibility.Collapsed;
+                            break;
+                        }
+                    }
+                }
             }
+            System.IO.File.AppendAllText(@"D:\sanu.txt", "Count: " + DataGridExpander.Columns.Count.ToString() + " Width: " + DataGridExpander.ActualWidth + "::::\n\r");
         }
         #endregion Methods
+
+        public ObservableCollection<Person> People { get; set; }
+        public List<ExpandableGridColumn> Columns { get; set; }
+    }
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+
     }
 }
